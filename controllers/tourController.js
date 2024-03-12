5f0750c293475307232a52aa18ad749edc8cbce0
Jyotiraditya <jyotiraditya602@gmail.com>
2024-03-12 15:22:05 +0530

    modified:   controllers/tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,8 +3,21 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   console.log('hi');
   try {
-    const tourData = await Tour.find();
-    // const tours = JSON.parse(tourData);
+    // BUILD QUERY
+    // 1)Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+    
+
+    // 2)Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const tourData = await query;
     res.status(200).json({
       status: 'success',
       tourData
