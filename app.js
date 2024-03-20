34834aacc6be5c725316e8297a584466f7a86feb
Jyotiraditya <jyotiraditya602@gmail.com>
2024-03-20 17:23:21 +0530

    After section 10: Ep 131

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+  next();
+})
+
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
